c8fdd2fcb688e4ef5ee19c049e817c459dce60bd
YuDavidCao <yu.cao20041208@gmail.com>
2024-12-14 09:46:18 -0500

    Initial Commit

---
+++ ClipboardHistory/ClipboardHistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipboardHistoryApp.swift
+//  ClipboardHistory
+//
+//  Created by Yu Cao on 12/14/24.
+//
+
+import SwiftUI
+
+@main
+struct ClipboardHistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClipboardHistoryTests/ClipboardHistoryTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClipboardHistoryTests.swift
+//  ClipboardHistoryTests
+//
+//  Created by Yu Cao on 12/14/24.
+//
+
+import Testing
+@testable import ClipboardHistory
+
+struct ClipboardHistoryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
